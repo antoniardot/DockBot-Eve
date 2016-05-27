7c12c0f2c61385ca883f4462b80e79c1b7996e9a
Kim Reichert <kimrdot@gmail.com>
2016-05-27 16:03:22 +0200

    First setup of eve project with lejos library and a testClass

---
+++ src/test/TestEveSetup.java
@@ -0,0 +1,18 @@
+package test;
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+
+
+/**
+ * This class can be run and used to see if eve is connected corretly.
+ * 
+ * @author Kim
+ *
+ */
+public class TestEveSetup {
+	public static void main(String[] args) {
+		LCD.drawString("Plugin Test", 0, 4);
+		Delay.msDelay(5000);
+	}
+
+}
